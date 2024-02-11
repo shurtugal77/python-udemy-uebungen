4155e2c52ef2d0b03223eb24e20804a3819249f0
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-11 16:36:55 +0100

    added checksum calculator

---
+++ Day_2/day2_dataTypes.py
@@ -0,0 +1,32 @@
+# Data Types
+# String
+
+testString = "Hello"
+firstLetter = testString[0]
+lastLetter = testString[len(testString)-1]
+
+print(firstLetter)
+print(lastLetter)
+
+# Integer
+
+number = 123_456_789
+print(number)
+
+# Float
+
+pi = 3.14159
+print(pi)
+
+# Boolean
+
+yourCode = False
+myCode = True
+
+# Typecheck
+print(type(myCode))
+
+# Type conversion = casting
+str(yourCode)
+
+
---
+++ Day_2/day2_twoDigitNumber.py
@@ -0,0 +1,7 @@
+# Quersummenrechner
+number = 39
+firstDigit = str(number)[0]
+secondDigit = str(number)[1]
+checkSum = int(firstDigit) + int(secondDigit)
+
+print(checkSum)
