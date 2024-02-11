c511f9f6d1e16316e43a379fe0103c1430a89849
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-11 15:31:34 +0100

    updated band name generator project

--- Day_1/day1_project.py
+++ Day_1/day1_project.py
@@ -3,11 +3,11 @@
 print("Hello fellow user - now running 'Band Name Generator v0.0.1'")
 
 # City you grew up in
-userCity = input("Please tell me the name of the city you grew up in")
+userCity = input("Please tell me the name of the city you grew up in?\n")
 
 # Name of a pet
-userPetName = input("Now please give me your favorit name of a pet of yours")
+userPetName = input("Now please give me your favorit name of a pet of yours\n")
 
 # Combine city name and pet name and show band name
 bandName = userCity + " " + userPetName
-print("Your sick new band name is:" + bandName)
+print("Your sick new band name is:" + " " + bandName  +" " + "69")
