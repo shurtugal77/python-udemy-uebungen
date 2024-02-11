194c81aa87aff12824909059497362cde3da34ae
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-11 12:34:08 +0100

    added input code

---
+++ Days/day1_input.py
@@ -0,0 +1,15 @@
+# Theorie
+# name = input("What is your name?")
+# print ("Hello, mah ballz " + name)
+
+# name1 = input("What is your name?")
+# print(name1)
+# name2 = input()
+# print(name2)
+
+number1 = int(input("Enter a number"))
+number2 = int(input("Enter another number"))
+sum = number1 * number2
+sumstr = str(sum)
+print("Your product is: " + sumstr)
+
