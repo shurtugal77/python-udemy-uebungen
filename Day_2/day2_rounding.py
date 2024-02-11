961b1417c605c1a29336f891336a582521878257
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-11 17:07:50 +0100

    added datatype handling and fstrings

---
+++ Day_2/day2_rounding.py
@@ -0,0 +1,21 @@
+a = 8
+b = 3
+gerundet = round(a / b, 6)
+
+print(gerundet)
+print(type(gerundet))
+
+# Integer beim Teilem
+
+print(a // b)
+
+# Erneute Benutzung von Oepratoren
+result = 4 / 2
+result /= 2
+print(result)
+
+# fStrings
+score = 0
+height = 1.0
+isWinning = True
+print(f"Your score is {score}, your height is {height}. But are you winning? This is: {isWinning}")
