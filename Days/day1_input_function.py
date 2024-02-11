5bedd4d87dad1814793f440f2b5bb13584fdbe7f
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-11 12:43:37 +0100

    added input function

---
+++ Days/day1_input_function.py
@@ -0,0 +1,7 @@
+#Function to input a name and print out the length of the string
+
+yourName = input("What is your name?")
+yourNameLength = len(yourName)
+intOfLength = str(yourNameLength)
+
+print("By the way - the length of the string of your name is" + " " + intOfLength)
