711f791f91a0fe3f659ae52e801e93e506bb143b
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-18 13:10:16 +0100

    added a tip calculator

---
+++ Day_2/day2_tipCalculator.py
@@ -0,0 +1,16 @@
+# Total bill
+tableBill = float(input("Für wie viel Geld habt ihr denn schon wieder Sushi gegessen?\n"))
+#tableBill = 124.56
+
+# Percentage tip
+tipPercentage = int(input("Wie viel Trinkggeld darf es sein? 10, 12 oder 15%\n"))
+tipFloat = 1 + tipPercentage / 100
+
+# How many people to split the bill with
+peopleAmount = int(input("Wie viele Leute haben sich den Ranzen vollgehauen?\n"))
+
+# Each person should pay
+totalBill = tableBill * tipFloat
+billPerPerson = round(totalBill / peopleAmount, 2)
+
+print(f"Jeder von euch Nassbirnen zahlt jetzt bitte mal ${billPerPerson}")
