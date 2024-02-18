5e29edc228788f7a44f068435b491215e07e90f6
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-18 14:00:28 +0100

    added even-odd check

---
+++ Day_3/day3_conditions.py
@@ -0,0 +1,20 @@
+# print("Welcome to the rollercoaster!")
+# height = int(input("What is your height in cm?\n"))
+
+# # if condition check mit ==, statt = 
+
+# if height > 120:
+#     print("You can ride the rollercoaster!")
+# else:
+#     print("Sorry, you have to grow taller before you can ride.")
+
+# Even / Odd check
+
+yourNumber = int(input("Tell me your number as integer.\n"))
+
+divisonModulo = yourNumber % 2
+
+if divisonModulo != 0:
+    print("Your number is odd")
+else:
+    print("Your number is even")
