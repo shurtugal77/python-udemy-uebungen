4a11c0c49aeb19d435987ef176df55e4144fa5a1
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-25 15:49:24 +0100

    advanced treasuremap with nested lists

--- Day_4/day4_advancedTreasureMap.py
+++ Day_4/day4_advancedTreasureMap.py
@@ -7,9 +7,31 @@ map = [line1, line2, line3]
 print("Hiding your treasure! X marks the spot.")
 position = input("Wo soll der Schatz versteckt werden Captain?\n") # Where do you want to put the treasure
 
+#Check row number
+tresNumber = int()
+if position[1] == "1":
+    tresNumber = 1
+elif position[1] == "2":
+    tresNumber = 2
+elif position[1] == "3":
+    tresNumber = 3
+
+#print(tresNumber)
+
+#Check column character
+tresChar = int()
 if position[0] == "A":
-    print("Workz")
-else:
+    tresChar = 1 
+elif position[0] == "B":
+    tresChar = 2
+elif position[0] == "C":
+    tresChar = 3
+
+#print(tresChar)
+
+#Set "X" on map
+map[tresNumber-1][tresChar-1] = "X"
 
 
+# Check map
 print(f"{line1}\n{line2}\n{line3}")
