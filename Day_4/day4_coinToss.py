a07e2903dbf8bc3d8683a9c7c9df918e84543ed5
Robert Hohenester <robert.hohenester@googlemail.com>
2024-02-25 13:20:26 +0100

    added random generator

---
+++ Day_4/day4_coinToss.py
@@ -0,0 +1,16 @@
+import random
+
+randToss = random.randint(0,1)
+
+if randToss == 0:
+    print("Tails")
+elif randToss == 1:
+    print("Heads")
+
+
+isHead = random.randint(0,1)
+if isHead:
+    print("Tails")
+else:
+    print("Heads")
+
---
+++ Day_4/day4_random.py
@@ -0,0 +1,11 @@
+import random
+
+# randomInteger = random.randint(1, 10)
+# print(randomInteger)
+
+
+randomFloat = 0 + random.random() * (5-0)
+print(randomFloat)
+
+loveScore = random.randint(1, 100)
+print(f"Your love score is {loveScore}")
