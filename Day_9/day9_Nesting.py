988e13c291b10f0e2abe6dbac16fadf13ca1d99b
Robert Hohenester <robert.hohenester@googlemail.com>
2024-03-22 16:54:58 +0100

    added nesting and travellog

---
+++ Day_9/day9_Nesting.py
@@ -0,0 +1,38 @@
+# Nesting lists
+
+traveLog = {
+    "France": ["Paris", "Lille", "Dijon"],
+}
+
+# Nesting dictionaries
+
+travelLog = {
+    "France": {
+        "visited": "Paris",
+        "notVisited": ["Lille", "Dijon"]
+    },
+    "Germany": ["Berlin", "Stuttgart"]
+}
+
+
+pythonLovers = [
+    {
+        "name": "Robert",
+        "age": 18,
+        "hobbies": ["gaming", "deez", "nuts"],
+        "education": {
+            "elementary": True,
+            "highschool": False,
+            "college": True,
+            "Nussschule": True
+        }
+    },
+
+    {
+        "name": "Rebecca",
+        "age": 31,
+        "hobbies": False
+    }
+
+]
+
