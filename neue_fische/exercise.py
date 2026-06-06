81b2bcd1abfb191f9c71d649f37b0efe366708b1
Robert Hohenester <robert.hohenester@googlemail.com>
2026-06-06 15:40:25 +0200

    add python lists examples

--- neue_fische/exercise.py
+++ neue_fische/exercise.py
@@ -19,3 +19,24 @@ print(x)
 uiToll()
 print(x)
 
+
+# Length of lists
+itsMyList = ["deez", "nuts", 3, True]
+print(len(itsMyList))
+
+
+# Creating a list with the list constructor
+thisList = list(("Apfel", "Banane", "Grapefruit"))
+print(thisList)
+
+
+# Accessing list items with range
+rangeList = ["rot", "blau", "grün", "gelb", "orange", "lila", "schwarz"]
+print(rangeList[1:5])
+# 1 included : 5 excluded
+
+# Check if item exists in list
+checkList = ["apple", "banana", "cherry"]
+if "apple" in checkList:
+    print("Ja Junge, dat is dabei")
+
